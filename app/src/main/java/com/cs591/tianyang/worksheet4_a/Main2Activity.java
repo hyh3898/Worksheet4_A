75adab62e3f6ad7b9beebcabd7813eb7977f0f4f
gentlemanZ <tianyangzhong2013@gmail.com>
2016-10-02 21:20:32 -0400

    register the imageView in mainactivity2.

--- app/src/main/java/com/cs591/tianyang/worksheet4_a/Main2Activity.java
+++ app/src/main/java/com/cs591/tianyang/worksheet4_a/Main2Activity.java
@@ -2,12 +2,17 @@ package com.cs591.tianyang.worksheet4_a;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private
+    ImageView ImageMove;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //register ImageView
+        ImageMove = (ImageView) findViewById(R.id.ImageMove);
+
     }
 }
